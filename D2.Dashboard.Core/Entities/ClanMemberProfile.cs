101c4260e4842554a4c2c12e483f5b8d6e0f3854
Daniel Worthy <danieljworthy@gmail.com>
2018-12-01 22:08:39 -0600

    Updates around new models

---
+++ D2.Dashboard.Core/Entities/ClanMemberProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D2.Dashboard.Core.Entities
+{
+    public class ClanMemberProfile
+    {
+        /*
+         * 100 component
+         * lastPlayed 
+         * 
+        Triumph Score
+
+         */
+    }
+}
---
+++ D2.Dashboard.Core/Entities/ClanMemberStats.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D2.Dashboard.Core.Entities
+{
+    public class ClanMemberStats
+    {
+        /*
+         /PVE/
+        pvekills
+        secondsPlayed
+        efficiency
+        precisionKills
+        heroicPublicEventsCompleted
+        adventuresCompleted
+        allParticipantsTimePlayed
+        publicEventsCompleted
+        highestLightLevel
+
+        /pvp/
+        activitiesEntered
+        activitiesWon
+        */
+    }
+}
