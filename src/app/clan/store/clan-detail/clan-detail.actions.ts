409a3793b5fd84d008f17a70bbfb686cb362fbd5
WorthyD <danieljworthy@gmail.com>
2019-07-11 06:10:11 -0500

    Updates

---
+++ src/app/clan/store/clan-detail/clan-detail.actions.ts
@@ -0,0 +1,13 @@
+import { Action } from '@ngrx/store';
+
+import { Clan } from 'bungie-parse';
+
+export const LOAD_CLANS = ``;
+export const LOAD_CLANS_FAIL = ``;
+export const LOAD_CLANS_SUCCESS = ``;
+
+
+
+export const ADD_CLAN = ``;
+export const ADD_CLAN_FAIL = ``;
+export const ADD_CLAN_SUCCESS = ``;
