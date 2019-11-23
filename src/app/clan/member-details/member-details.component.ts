adf39f1cb3bdbb95fa03215ba0ef28e521de703d
WorthyD <danieljworthy@gmail.com>
2019-11-22 20:41:53 -0600

    Calling it a night because the api is down

--- src/app/clan/member-details/member-details.component.ts
+++ src/app/clan/member-details/member-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { distinctUntilChanged, map, takeUntil, take, filter } from 'rxjs/operators';
-import { Store, select } from '@ngrx/store';
+import { Store, select, createSelector } from '@ngrx/store';
 
 import { ClanMember, MemberProfile, MemberActivityStat } from 'bungie-models';
 
@@ -13,24 +13,26 @@ import * as memberProfileSelectors from '../store/member-profiles/member-profile
 import * as memberActivitySelectors from '../store/member-activities/member-activities.selectors';
 import * as memberActivityActions from '../store/member-activities/member-activities.actions';
 
+export const getRecentClanMemberActivities = memberId =>
+    createSelector(memberActivitySelectors.getClanMemberActivities(memberId), activities => {
+        if (!memberId || !activities) {
+            return null;
+        }
+
+        return activities.sort((a, b) => a.period.getTime() - b.period.getTime()).slice(0, 10);
+    });
+
 @Component({
     selector: 'app-member-details',
     templateUrl: './member-details.component.html',
     styleUrls: ['./member-details.component.scss']
 })
 export class MemberDetailsComponent implements OnInit, OnDestroy {
-    constructor(
-        private activatedRoute: ActivatedRoute,
-        private store: Store<any>
-    ) {
-        this.memberId
-            .pipe(takeUntil(this.destroyed))
-            .subscribe(r => this.loadMemberDetails(r));
+    constructor(private activatedRoute: ActivatedRoute, private store: Store<any>) {
+        this.memberId.pipe(takeUntil(this.destroyed)).subscribe(r => this.loadMemberDetails(r));
     }
 
-    private memberId = this.activatedRoute.params.pipe(
-        map(x => x.memberId, distinctUntilChanged())
-    );
+    private memberId = this.activatedRoute.params.pipe(map(x => x.memberId, distinctUntilChanged()));
 
     member$: Observable<ClanMember>;
     profile$: Observable<MemberProfile>;
@@ -41,26 +43,19 @@ export class MemberDetailsComponent implements OnInit, OnDestroy {
     ngOnInit() {}
 
     loadMemberDetails(memberId) {
-        this.member$ = this.store.pipe(
-            select(clanMemberSelectors.getClanMemberById(memberId))
-        );
-        this.profile$ = this.store.pipe(
-            select(memberProfileSelectors.getClanMemberById(memberId))
-        );
+        this.member$ = this.store.pipe(select(clanMemberSelectors.getClanMemberById(memberId)));
+        this.profile$ = this.store.pipe(select(memberProfileSelectors.getClanMemberById(memberId)));
 
-        this.activities$ = this.store.pipe(
-            select(memberActivitySelectors.getClanMemberActivities(memberId))
-        );
+        this.activities$ = this.store.pipe(select(getRecentClanMemberActivities(memberId)));
 
-        this.profile$.pipe(
-            filter(loaded => !!loaded),
-            take(1)
-            ).subscribe(x => {
-            console.log(x);
-            this.store.dispatch(
-                memberActivityActions.loadMemberActivities({ member: x })
-            );
-        });
+        this.profile$
+            .pipe(
+                filter(loaded => !!loaded),
+                take(1)
+            )
+            .subscribe(x => {
+                this.store.dispatch(memberActivityActions.loadMemberActivities({ member: x }));
+            });
     }
 
     ngOnDestroy() {
