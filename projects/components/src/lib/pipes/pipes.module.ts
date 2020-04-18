b2d35c768cc4d4b6efb80138c0c7f7b2048936bb
WorthyD <danieljworthy@gmail.com>
2020-04-18 07:12:46 -0500

    Adding pipes

--- projects/components/src/lib/pipes/pipes.module.ts
+++ projects/components/src/lib/pipes/pipes.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DateAgoPipe } from './date-ago.pipe';
-
-
+import { ClassTypeTextPipe } from './class-type/class-type-text.pipe';
+import { RaceTypeTextPipe } from './race-type/race-type-text.pipe';
+import { GenderTypeTextPipe } from './gender-type/gender-type-text.pipe';
 
 @NgModule({
-  declarations: [DateAgoPipe],
-  imports: [
-    CommonModule
-  ]
+    declarations: [DateAgoPipe, ClassTypeTextPipe, RaceTypeTextPipe, GenderTypeTextPipe],
+    exports: [DateAgoPipe, ClassTypeTextPipe, RaceTypeTextPipe, GenderTypeTextPipe],
+    imports: [CommonModule],
 })
-export class PipesModule { }
+export class PipesModule {}
