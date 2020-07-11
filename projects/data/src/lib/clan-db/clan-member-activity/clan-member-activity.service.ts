ee1493268eb8d31eab9fd332649dc7e5397bc2d8
WorthyD <danieljworthy@gmail.com>
2020-07-11 18:55:43 -0500

    stuff

--- projects/data/src/lib/clan-db/clan-member-activity/clan-member-activity.service.ts
+++ projects/data/src/lib/clan-db/clan-member-activity/clan-member-activity.service.ts
@@ -4,18 +4,29 @@ import { Destiny2Service } from 'bungie-api';
 import { BaseClanService } from '../base-clan.service';
 import { ClanDatabase } from '../ClanDatabase';
 import { MemberProfile } from 'bungie-models';
+import { from, of } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 
 @Injectable()
-export class ClanMemberActivityService extends BaseClanService{
+export class ClanMemberActivityService extends BaseClanService {
   constructor(private d2Service: Destiny2Service, private clanDB: ClanDatabase) {
     super(clanDB, 'MemberActivities');
   }
-  private getMemberActivityId(){
+  private getMemberActivityId() {}
+
+  private getMemberCharacterActivity(member: MemberProfile, characterId: number) {
+    return of(false);
   }
 
-
-  private getMemberActivitiesSerialized(clanId: number, member: MemberProfile){
+  private getMemberCharacterActivitySerialized(clanId: number, member: MemberProfile, characterId: number) {
+    return of(false);
   }
-
-
+  private getMemberActivity(clanId: number, member: MemberProfile) {
+    return from(member.profile.data.characterIds).pipe(
+      mergeMap((characterId) => {
+        return this.getMemberCharacterActivitySerialized(clanId, member, characterId);
+      })
+    );
+  }
+  getMemberActivitiesSerialized(clanId: number, member: MemberProfile) {}
 }
