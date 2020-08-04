d14d96003ac094325dc3afb0c576d08c37a3f2f6
WorthyD <danieljworthy@gmail.com>
2020-08-04 07:45:10 -0500

    updates

---
+++ projects/data/src/lib/clan-db/member-activity-stats/member-activity-stat.serializer.ts
@@ -0,0 +1,12 @@
+import { MemberProfile } from 'bungie-models';
+import { DestinyHistoricalStatsDestinyAggregateActivityStats } from 'bungie-api';
+
+// export function memberActivityStatSerializer(
+//   records: DestinyHistoricalStatsDestinyAggregateActivityStats[],
+//   recordHashes: number[]
+// ): DestinyHistoricalStatsDestinyAggregateActivityStats[] {
+//   let returnRecords = recordHashes.map(hash => {
+//     return
+//   });
+
+// }
--- projects/data/src/lib/clan-db/member-activity-stats/member-activity-stats.service.ts
+++ projects/data/src/lib/clan-db/member-activity-stats/member-activity-stats.service.ts
@@ -3,21 +3,47 @@ import { Destiny2Service } from 'bungie-api';
 import { ClanDatabase } from '../ClanDatabase';
 import { from, of } from 'rxjs';
 import { BaseClanService } from '../base-clan.service';
-import { map, catchError, switchMap } from 'rxjs/operators';
+import { map, catchError, switchMap, mergeMap } from 'rxjs/operators';
 import { StoreId } from '../app-indexed-db';
 import { ClanMember, MemberProfile } from 'bungie-models';
 
 @Injectable()
 export class MemberActivityStatsService extends BaseClanService {
   constructor(private d2Service: Destiny2Service, private clanDb: ClanDatabase) {
-    super(clanDb, StoreId.ClanDetails);
+    super(clanDb, StoreId.MemberActivityStats);
   }
-
+  private getMemberActivityStatId(member: MemberProfile, characterId: number) {
+    return `${member.profile.data.userInfo.membershipType}-${member.profile.data.userInfo.membershipId}-${characterId}`;
+  }
   private getMemberActivityStatsFromAPI(member: MemberProfile, characterId: number) {
     return this.d2Service.destiny2GetDestinyAggregateActivityStats(
       characterId,
       member.profile.data.userInfo.membershipId,
       member.profile.data.userInfo.membershipType
+    );
+  }
+  private getMemberActivityStats(clanId: number, member: MemberProfile, characterId: number) {
+    const characterActivityStatId = this.getMemberActivityStatId(member, characterId);
+    return from(this.getDataFromCache(clanId.toString(), characterActivityStatId)).pipe(
+      mergeMap((cachedData) => {
+        if (this.isCacheValid(cachedData, 0, new Date(member.profile.data.dateLastPlayed))) {
+          return of(cachedData.data);
+        }
+        return this.getMemberActivityStatsFromAPI(member, characterId).pipe(
+          map((activityStatResponse) => {
+            if (activityStatResponse.Response) {
+              this.updateDB(clanId, characterActivityStatId, activityStatResponse.Response);
+              return activityStatResponse.Response;
+            }
+          }),
+          catchError((error) => {
+            if (cachedData && cachedData.data) {
+              return of(cachedData.data);
+            }
+            throw error;
+          })
+        );
+      })
     );
   }
 }
