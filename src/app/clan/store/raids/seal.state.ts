5795379983132fa8de79b829975b9ca0589356ab
WorthyD <danieljworthy@gmail.com>
2020-08-06 22:24:05 -0500

    finished stuff

---
+++ src/app/clan/store/raids/seal.state.ts
@@ -0,0 +1,21 @@
+import {  SealMembers } from 'bungie-models';
+
+import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
+import {  } from '@destiny/data';
+
+
+
+export interface SealState extends EntityState<SealMembers> {
+  loaded: boolean;
+  loading: boolean;
+}
+
+export const SealProfileAdapter: EntityAdapter<SealMembers> = createEntityAdapter<SealMembers>({
+  selectId: (cm: SealMembers) => cm.seal.hash,
+  sortComparer: false
+});
+
+export const SealInitialState: SealState = SealProfileAdapter.getInitialState({
+  loaded: false,
+  loading: false
+});
