705a24efef9ec7fe5da3e2f220053e4e3ad1e0d0
WorthyD <danieljworthy@gmail.com>
2020-08-16 19:55:30 -0500

    updates

--- projects/data/src/lib/clan-db/member-activity-stats/member-activity-stats.service.spec.ts
+++ projects/data/src/lib/clan-db/member-activity-stats/member-activity-stats.service.spec.ts
@@ -12,7 +12,7 @@ import { of, defer } from 'rxjs';
 import { ClanMember, MemberProfile } from 'bungie-models';
 import { HttpErrorResponse } from '@angular/common/http';
 
-describe('MemberActivityStatsService', () => {
+fdescribe('MemberActivityStatsService', () => {
   let service: MemberActivityStatsService;
   let dbService: ClanDatabase;
   let d2Service: Destiny2Service;
@@ -65,7 +65,7 @@ describe('MemberActivityStatsService', () => {
         });
     });
 
-    it('should get profile from DB and not call service if cache is good', () => {
+    fit('should get profile from DB and not call service if cache is good', () => {
       const dbGetSpy = spyOn(dbService, 'getById').and.callFake((repo, store, id) => {
         const m = MOCK_DB_ACTIVITY_STATS.find((x) => x.id === id);
         return of(m);
