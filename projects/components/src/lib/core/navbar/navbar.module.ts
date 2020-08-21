ddfe55f4932018b5fe47604c8c7902c5fe5e86c2
WorthyD <danieljworthy@gmail.com>
2020-08-20 21:01:19 -0500

    adding themeting'

--- projects/components/src/lib/core/navbar/navbar.module.ts
+++ projects/components/src/lib/core/navbar/navbar.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MaterialModule } from '../../shared/modules/material.module';
 @NgModule({
-    declarations: [NavbarComponent],
-    exports: [NavbarComponent],
-    imports: [CommonModule, MatToolbarModule]
+  declarations: [NavbarComponent],
+  exports: [NavbarComponent],
+  imports: [CommonModule, MatToolbarModule, MaterialModule]
 })
 export class NavbarModule {}
