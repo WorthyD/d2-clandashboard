b7c30b9c3a78623cd6e069c590b204081a867ba6
WorthyD <danieljworthy@gmail.com>
2020-09-13 15:39:35 -0500

    adding new files

---
+++ projects/components/src/lib/clan-roster-activity-table/clan-roster-activity-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-clan-roster-activity-table',
+  templateUrl: './clan-roster-activity-table.component.html',
+  styleUrls: ['./clan-roster-activity-table.component.scss']
+})
+export class ClanRosterActivityTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/components/src/lib/clan-roster-activity-table/clan-roster-activity-table.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClanRosterActivityTableComponent } from './clan-roster-activity-table.component';
+
+
+
+@NgModule({
+  declarations: [ClanRosterActivityTableComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ClanRosterActivityTableModule { }
