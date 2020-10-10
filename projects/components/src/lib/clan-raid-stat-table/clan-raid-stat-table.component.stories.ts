d8c8872f62715eb04c26f9ac1190a502e8c6d269
WorthyD <danieljworthy@gmail.com>
2020-10-10 06:57:32 -0500

    feat: Adding better loading to raid activity

--- projects/components/src/lib/clan-raid-stat-table/clan-raid-stat-table.component.stories.ts
+++ projects/components/src/lib/clan-raid-stat-table/clan-raid-stat-table.component.stories.ts
@@ -5,9 +5,13 @@ import { MOCK_RAID_STATS } from './_MOCK_RAID_STATS';
 import { ClanRaidStatTableModule } from './clan-raid-stat-table.module';
 import { ClanRaidStatTableComponent } from './clan-raid-stat-table.component';
 
+import { withKnobs, boolean } from '@storybook/addon-knobs';
+
 export default {
   title: 'Clan / Clan Raid Stats',
   decorators: [
+
+    withKnobs,
     moduleMetadata({
       imports: [StorybookModule, ClanRaidStatTableModule]
     })
@@ -27,5 +31,14 @@ export const isLoading = () => ({
   template: `<lib-clan-raid-stat-table [memberRaidStats]="raidStats" isLoading="true"></lib-clan-raid-stat-table>`,
   props: {
     raidStats: MOCK_RAID_STATS
+  }
+});
+
+export const isLoadingToggle = () => ({
+  component: ClanRaidStatTableComponent,
+  template: `<lib-clan-raid-stat-table [memberRaidStats]="raidStats" [isLoading]="isLoading"></lib-clan-raid-stat-table>`,
+  props: {
+    raidStats: MOCK_RAID_STATS,
+    isLoading: boolean('Is Loading', true)
   }
 });
