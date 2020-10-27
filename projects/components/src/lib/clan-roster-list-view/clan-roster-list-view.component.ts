2214a18ca976ff659a205bf634d02b0b69144c31
WorthyD <danieljworthy@gmail.com>
2020-10-26 19:00:06 -0500

    Stuff

--- projects/components/src/lib/clan-roster-list-view/clan-roster-list-view.component.ts
+++ projects/components/src/lib/clan-roster-list-view/clan-roster-list-view.component.ts
@@ -1,12 +1,4 @@
-import {
-    Component,
-    OnInit,
-    Input,
-    Output,
-    EventEmitter,
-    ViewChild,
-    ChangeDetectionStrategy,
-} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ChangeDetectionStrategy } from '@angular/core';
 
 import { MemberProfile, ClanMember } from 'bungie-models';
 import { MatSort } from '@angular/material/sort';
@@ -17,88 +9,84 @@ import { compare } from '../utilities/compare';
 
 export { ClanMemberListItem } from '../models/ClanMemberListItem';
 
-
 @Component({
-    selector: 'lib-clan-roster-list-view',
-    templateUrl: './clan-roster-list-view.component.html',
-    styleUrls: ['./clan-roster-list-view.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'lib-clan-roster-list-view',
+  templateUrl: './clan-roster-list-view.component.html',
+  styleUrls: ['./clan-roster-list-view.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ClanRosterListViewComponent {
-    displayedColumns: string[] = [
-        'displayName',
-        'characters',
-        'powerBonus',
-        'joinDate',
-        'dateLastPlayed',
-        'controls',
-    ];
-    sortedData: ClanMemberListItem[];
+  displayedColumns: string[] = [
+    'memberType',
+    'displayName',
+    'characters',
+    'powerBonus',
+    'joinDate',
+    'dateLastPlayed',
+    'controls'
+  ];
+  sortedData: ClanMemberListItem[];
 
-    _members: ClanMemberListItem[];
+  _members: ClanMemberListItem[];
 
-    @Input()
-    get members(): ClanMemberListItem[] {
-        return this._members;
+  @Input()
+  get members(): ClanMemberListItem[] {
+    return this._members;
+  }
+  set members(value) {
+    this._members = value;
+    this.sortedData = value.slice();
+  }
+
+  @Output() viewMember = new EventEmitter<ClanMember>();
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  constructor() {}
+
+  sortData(sort: Sort) {
+    const data = this.members.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
     }
-    set members(value) {
-        this._members = value;
-        this.sortedData = value.slice();
-    }
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'displayName':
+          return compare(
+            a.member.destinyUserInfo.displayName.toLowerCase(), // TODO make sure this doesn't break.
+            b.member.destinyUserInfo.displayName.toLowerCase(),
+            isAsc
+          );
+        case 'dateLastPlayed':
+          return compare(a.profile?.profile.data.dateLastPlayed, b.profile?.profile.data.dateLastPlayed, isAsc);
+        case 'powerBonus':
+          return compare(
+            a.profile?.profileProgression?.data?.seasonalArtifact?.powerBonus ?? 0,
+            b.profile?.profileProgression?.data?.seasonalArtifact?.powerBonus ?? 0,
+            isAsc
+          );
+        case 'joinDate':
+          return compare(a.member?.joinDate, b.member?.joinDate, isAsc);
 
-    @Output() viewMember = new EventEmitter<ClanMember>();
+        case 'characters':
+          return compare(this.getHighestLight(a), this.getHighestLight(b), isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
 
-    @ViewChild(MatSort, { static: true }) sort: MatSort;
+  // This is probably terribly un-performant, but whatever
+  getHighestLight(item: ClanMemberListItem) {
+    const lights = item.profile.profile.data.characterIds.map((hash) => {
+      return item.profile.characters.data[hash].light;
+    });
+    return Math.max(...lights);
+  }
 
-    constructor() {}
-
-    sortData(sort: Sort) {
-        const data = this.members.slice();
-        if (!sort.active || sort.direction === '') {
-            this.sortedData = data;
-            return;
-        }
-        this.sortedData = data.sort((a, b) => {
-            const isAsc = sort.direction === 'asc';
-            switch (sort.active) {
-                case 'displayName':
-                    return compare(
-                        a.member.destinyUserInfo.displayName.toLowerCase(), // TODO make sure this doesn't break.
-                        b.member.destinyUserInfo.displayName.toLowerCase(),
-                        isAsc
-                    );
-                case 'dateLastPlayed':
-                    return compare(
-                        a.profile?.profile.data.dateLastPlayed,
-                        b.profile?.profile.data.dateLastPlayed,
-                        isAsc
-                    );
-                case 'powerBonus':
-                    return compare(
-                        a.profile?.profileProgression?.data?.seasonalArtifact?.powerBonus ?? 0,
-                        b.profile?.profileProgression?.data?.seasonalArtifact?.powerBonus ?? 0,
-                        isAsc
-                    );
-                case 'joinDate':
-                    return compare(a.member?.joinDate, b.member?.joinDate, isAsc);
-
-                case 'characters':
-                    return compare(this.getHighestLight(a), this.getHighestLight(b), isAsc);
-                default:
-                    return 0;
-            }
-        });
-    }
-
-    // This is probably terribly un-performant, but whatever
-    getHighestLight(item: ClanMemberListItem) {
-        const lights = item.profile.profile.data.characterIds.map((hash) => {
-            return item.profile.characters.data[hash].light;
-        });
-        return Math.max(...lights);
-    }
-
-    memberClick(m: ClanMemberListItem) {
-        this.viewMember.emit(m.member);
-    }
+  memberClick(m: ClanMemberListItem) {
+    this.viewMember.emit(m.member);
+  }
 }
