598ae32424c0c8b954035faf44ce8983aa7ebdc5
WorthyD <danieljworthy@gmail.com>
2020-11-22 05:42:37 -0600

    feat: Deep Stone Crypt Addition
    Adding Deep Stone Crypt to the raid list

---
+++ projects/bungie-models/src/lib/entities/raids/deep-stone-crypt.ts
@@ -0,0 +1,23 @@
+
+import { Raid } from '../../models/Raid';
+
+export const DeepStoneCrypt: Raid = {
+  hashes: [910380154],
+  key: 'dsc',
+  abbreviatedName: 'DSC',
+  displayName: 'Deep Stone Crypt',
+  sortOrder: 18,
+  isGuidedGames: false
+};
+
+export const DeepStoneCryptGG: Raid = {
+  hashes: [3976949817],
+  key: 'dscgg',
+  abbreviatedName: 'DSC GG',
+  displayName: 'Deep Stone Crypt Guided Games',
+  sortOrder: 19,
+  isGuidedGames: true
+};
+
+//910380154
+// 3976949817
