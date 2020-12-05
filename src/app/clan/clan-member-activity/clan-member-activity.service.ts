919701de2df482699407ea91d00e69c805755386
WorthyD <danieljworthy@gmail.com>
2020-12-04 20:21:55 -0600

    prpgress

--- src/app/clan/clan-member-activity/clan-member-activity.service.ts
+++ src/app/clan/clan-member-activity/clan-member-activity.service.ts
@@ -4,21 +4,25 @@ import { ClanState } from '../store/clan-state.state';
 import { Store, select } from '@ngrx/store';
 import { loadClanMembersActivities } from '../store/member-recent-activities/member-recent-activities.actions';
 import * as clanIdSelectors from '../store/clan-id/clan-id.selector';
-import { getMemberActivityEntities, getAllMemberActivities,getClanMemberActivitiesLoading } from '../store/member-recent-activities/member-recent-activities.selectors';
+import {
+  getMemberActivityEntities,
+  getAllMemberActivities,
+  getClanMemberActivitiesLoading
+} from '../store/member-recent-activities/member-recent-activities.selectors';
 
 import { Observable, combineLatest, of } from 'rxjs';
-import { filter, take } from 'rxjs/operators';
+import { bufferTime, filter, map, mergeMap, switchMap, take, tap, toArray } from 'rxjs/operators';
 import {
   getMemberProfileEntities,
   getAllMembers,
   getIsMembersProfilesLoaded,
   getIsMembersProfilesLoading
 } from '../store/member-profiles/member-profiles.selectors';
-import { getMemberProfileId } from '@destiny/data';
+import { ClanMemberRecentActivityService, getMemberProfileId } from '@destiny/data';
 
 @Injectable()
 export class ClanMemberActivityService {
-  constructor(private store: Store<ClanState>) {}
+  constructor(private store: Store<ClanState>, private memberActivityService: ClanMemberRecentActivityService) {}
   isMembersLoaded$ = this.store.pipe(select(getIsMembersProfilesLoaded));
   clanId$ = this.store.select(clanIdSelectors.getClanIdState);
 
@@ -26,6 +30,29 @@ export class ClanMemberActivityService {
   areMembersLoading$ = this.store.pipe(select(getClanMemberActivitiesLoading));
   areMemberProfilesLoading$ = this.store.pipe(select(getIsMembersProfilesLoading));
   memberActivities$ = this.store.pipe(select(getAllMemberActivities));
+
+  isLoading = true;
+  preloadedInfo$ = combineLatest([this.isMembersLoaded$, this.clanId$, this.clanMemberProfiles$]).pipe(
+    filter(([isMembersLoaded, id, m]) => isMembersLoaded === true),
+    map((x) => {
+      return x;
+    })
+  );
+  activityStats = [];
+  activityStats$ = this.preloadedInfo$.pipe(
+    switchMap(([isMemberLoaded, id, clanMembers]) => {
+      this.isLoading = true;
+      this.memberActivityService.getSerializedProfilesActivity(id, clanMembers).pipe(
+        bufferTime(500, undefined, 20),
+        mergeMap((members) => {
+          this.activityStats = this.activityStats.concat({
+          });
+        }),
+        toArray(),
+        map((stats) => {})
+      );
+    })
+  );
 
   loadMemberActivity() {
     this.isMembersLoaded$
@@ -37,4 +64,6 @@ export class ClanMemberActivityService {
         this.store.dispatch(loadClanMembersActivities());
       });
   }
+
+  //  activityStats$ = this.
 }
