523a9be172c69077f319c906320c25c11a99241e
WorthyD <danieljworthy@gmail.com>
2020-12-24 20:20:13 -0600

    testing

--- projects/data/src/lib/stat-aggregators/clan-raids.service.spec.ts
+++ projects/data/src/lib/stat-aggregators/clan-raids.service.spec.ts
@@ -7,58 +7,7 @@ import { MemberActivityStatsService } from '../clan-db/member-activity-stats/mem
 import { Destiny2Service } from 'bungie-api-angular';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ClanDatabase } from '../clan-db';
-import { of } from 'rxjs';
-const MOCK_MEMBERS = [
-  {
-    profile: {
-      data: {
-        userInfo: {
-          membershipId: 1,
-          membershipType: 1
-        },
-        characterIds: [1, 2, 3]
-      }
-    }
-  },
-  {
-    profile: {
-      data: {
-        userInfo: {
-          membershipId: 2,
-          membershipType: 1
-        },
-        characterIds: [5, 6, 7]
-      }
-    }
-  }
-] as MemberProfile[];
 
-const MOCK_ACTIVITY = [
-  {
-    activityHash: 3089205900,
-    values: {
-      activityCompletions: { statId: 'activityCompletions', basic: { value: 0, displayValue: '0' } }
-    }
-  },
-  {
-    activityHash: 2693136600,
-    values: {
-      activityCompletions: { statId: 'activityCompletions', basic: { value: 1, displayValue: '1' } }
-    }
-  },
-  {
-    activityHash: 2693136601,
-    values: {
-      activityCompletions: { statId: 'activityCompletions', basic: { value: 7, displayValue: '7' } }
-    }
-  },
-  {
-    activityHash: 548750096,
-    values: {
-      activityCompletions: { statId: 'activityCompletions', basic: { value: 100, displayValue: '100' } }
-    }
-  }
-];
 describe('ClanRaidsService', () => {
   let service: ClanRaidsService;
   let activityService: MemberActivityStatsService;
@@ -74,17 +23,5 @@ describe('ClanRaidsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-  });
-  describe('getClanRaidStats', () => {
-    it('should return expected results', () => {
-      const serviceSpy = spyOn(activityService, 'getMemberCharacterActivityStatsSerialized').and.callFake(() => {
-        return of(MOCK_ACTIVITY);
-      });
-
-      service.getClanRaidStats(2, MOCK_MEMBERS).subscribe((x) => {
-        expect(x).toBeTruthy();
-        //console.log(x);
-      });
-    });
   });
 });
