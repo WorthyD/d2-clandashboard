897ef21acd39a321e11b59fdbbcac1cdb0aee23a
WorthyD <danieljworthy@gmail.com>
2021-01-17 18:54:47 -0600

    Chart updates

---
+++ projects/components/src/lib/shared/charts/bar-chart/bar-chart.component.stories.ts
@@ -0,0 +1,67 @@
+import { moduleMetadata } from '@storybook/angular';
+import { action } from '@storybook/addon-actions';
+
+import { BarChartModule } from './bar-chart.module';
+import { BarChartComponent } from './bar-chart.component';
+
+import { withKnobs, select, object, boolean } from '@storybook/addon-knobs';
+import { MOCK_GRID_ITEMS, REAL_DATA } from './_MOCK_GRID_ITEMS';
+import { MaterialModule } from '../../modules/material.module';
+import { LoadingModule } from '../../loading/loading.module';
+const events = [MOCK_GRID_ITEMS];
+
+export default {
+  title: 'Shared / Bar Chart',
+  decorators: [
+    withKnobs,
+    moduleMetadata({
+      imports: [BarChartModule, MaterialModule, LoadingModule],
+    })
+  ]
+};
+
+export const base = () => ({
+  component: BarChartComponent,
+  template: `
+  <div style="width:500px;">
+        <lib-activity-bar-chart [events]="allEvents[eventIndex]" (cellSelect)="cellSelect($event)"></lib-activity-bar-chart>
+        <hr>
+        <textarea style="width:100%; height:500px">
+        {{allEvents[eventIndex] | json}}
+        </textarea>
+  </div>
+   `,
+  props: {
+    allEvents: events,
+    eventIndex: select('DataSet', [0, 1], 0),
+    cellSelect: action('cell select')
+  }
+});
+
+export const realData = () => ({
+  component: BarChartComponent,
+  template: `
+  <div>
+        <lib-activity-bar-chart [events]="events" (cellSelect)="cellSelect($event)"></lib-activity-bar-chart>
+        <hr>
+        <textarea style="width:100%; height:500px">
+        </textarea>
+  </div>
+   `,
+  props: {
+    events: REAL_DATA
+  }
+});
+
+export const loadingTest = () => ({
+  component: BarChartComponent,
+  template: `
+  <div>
+        <lib-activity-bar-chart-story [events]="events" [isLoading]="isLoading"></lib-activity-bar-chart-story>
+  </div>
+   `,
+  props: {
+    events: REAL_DATA,
+    isLoading: boolean('Is Loading', true)
+  }
+});
---
+++ projects/components/src/lib/shared/charts/bar-chart/bar-chart.component.ts
@@ -0,0 +1,224 @@
+import {
+  Component,
+  OnInit,
+  Output,
+  Input,
+  ChangeDetectionStrategy,
+  ElementRef,
+  SimpleChanges,
+  OnChanges,
+  EventEmitter,
+  ViewEncapsulation,
+  ChangeDetectorRef
+} from '@angular/core';
+import * as d3 from 'd3';
+import { SECONDS_IN_HOUR } from '@destiny/models/constants';
+import { PlaytimePipe } from '../../../pipes/playtime/playtime.pipe';
+import { compare } from '../../../utilities/compare';
+
+@Component({
+  selector: 'lib-activity-bar-chart',
+  template: '',
+  styleUrls: ['./bar-chart.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
+})
+export class BarChartComponent implements OnInit {
+  hostElement;
+  svg;
+  data;
+  x;
+  y;
+  tooltip;
+
+  chartHeight = 500;
+  chartWidth = 600;
+  threshHold = SECONDS_IN_HOUR * 20;
+  color = d3.scaleLinear().range(['#00ff00', '#ff0000']).domain([0, 1]);
+
+  formatPipe = new PlaytimePipe();
+
+  _events;
+  @Input()
+  get events(): [] {
+    return this._events;
+  }
+
+  set events(value) {
+    if (value && value.length && value !== this._events) {
+      this._events = value;
+      this.updateChart(this._events);
+    }
+  }
+
+  @Input()
+  maxBarCount = 25;
+
+  @Input()
+  startDate: Date;
+
+  @Input()
+  endDate: Date;
+
+  constructor(private elRef: ElementRef, private cd: ChangeDetectorRef) {
+    this.hostElement = this.elRef.nativeElement;
+    this.cd.detach();
+
+    const date = new Date();
+    const offset = date.getDay() >= 2 ? 2 : -5;
+    this.endDate = new Date(date.setDate(date.getDate() - date.getDay() + offset));
+    this.startDate = new Date(new Date(this.endDate).setDate(new Date(this.endDate).getDate() - 182));
+  }
+
+  private updateChart(eventData) {
+    if (!this.svg) {
+      this.createChart(eventData);
+      return;
+    }
+  }
+
+  ngOnInit(): void {}
+
+  private createChart(eventData) {
+    this.removeExistingChartFromParent();
+    this.setChartDimensions();
+
+    this.addToolTip();
+
+    this.processData(eventData);
+  }
+
+  private removeExistingChartFromParent() {
+    // !!!!Caution!!!
+    // Make sure not to do;
+    //     d3.select('svg').remove();
+    // That will clear all other SVG elements in the DOM
+    d3.select(this.hostElement).select('svg').remove();
+  }
+
+  private setChartDimensions() {
+    this.svg = d3
+      .select(this.hostElement)
+      .attr('class', 'activity-heatmap')
+      .append('svg')
+      .attr('width', this.chartWidth)
+      .attr('height', this.chartHeight)
+      //.attr('viewBox', `0 0 ${this.chartWidth} ${this.chartHeight}`)
+      .append('g')
+      //.attr('transform', 'translate(' + 50 + ',' + 50 + ')');
+
+    //this.x = d3.scaleBand().range([0, this.chartWidth], 0.05);
+    this.x = d3.scaleBand().range([0, this.chartWidth]).padding(0.05);
+    this.y = d3.scaleLinear().range([this.chartHeight, 0]);
+  }
+  private addToolTip() {
+    this.tooltip = d3.select(this.hostElement).append('div').attr('class', 'activity-tooltip');
+  }
+  private processData(sourceData) {
+    if (sourceData) {
+      const cleanedData = this.prepData(sourceData);
+      this.x.domain(
+        cleanedData.map(function (d) {
+          return d.date;
+        })
+      );
+      this.y.domain([0, d3.max(cleanedData, (d) => d.seconds)]);
+
+      // this.svg
+      //   .append('g')
+      //   .attr('transform', 'translate(0,' + 100 + ')')
+      //   .call(d3.axisBottom(this.x));
+
+      this.svg
+        .append('g')
+        .call(
+          d3
+            .axisLeft(this.y)
+            .tickFormat(function (d) {
+              return d;
+            })
+            .ticks(10)
+        )
+        .append('text')
+        .attr('y', 6)
+        .attr('dy', '0.71em')
+        .attr('text-anchor', 'end')
+        .text('value');
+
+      const bars = this.svg.selectAll('bar').data(cleanedData).enter().append('rect');
+
+      bars
+        .attr('class', 'activity-bar')
+        .attr('x', (d) => {
+          return this.x(d.date);
+        })
+        .attr('y', (d) => {
+          return this.y(d.seconds);
+        })
+        .attr('width', this.x.bandwidth())
+        .attr('height', (d) => {
+          return this.chartHeight - this.y(d.seconds);
+        });
+
+      bars
+        .on('mouseover', (d) => {
+          this.tooltip.style('opacity', 0.9);
+          this.tooltip.html(`Week Starting: ${formatDate(d.date)}<br/> Time:  ${this.formatPipe.transform(d.seconds)}`);
+        })
+        .on('mousemove', () => {
+          this.tooltip.style('left', d3.event.pageX + 30 + 'px').style('top', d3.event.pageY + 'px');
+        })
+        .on('mouseout', (d) => {
+          this.tooltip.style('opacity', 0);
+        });
+    }
+  }
+
+  private prepData(sourceData) {
+    const preppedData = [];
+
+    for (let i = 1; i < 52; i++) {
+      // Needing to add 1 because of utc conversion i think.
+      const d = new Date(new Date(this.startDate).setDate(this.startDate.getDate() + i * 7));
+      if (d > this.endDate) {
+        break;
+      }
+      if (d >= this.startDate) {
+        const dFormatted = formatDate(d);
+
+        const data = sourceData.find((x) => x.date === dFormatted);
+        // console.log(d);
+        // console.log(dFormatted);
+        // console.log(sourceData.find((x) => x.date === dFormatted));
+
+        if (data) {
+          preppedData.push(data);
+        } else {
+          preppedData.push({ date: dFormatted, seconds: 0 });
+        }
+      }
+    }
+
+    preppedData.sort((a, b) => {
+      return compare(a.date, b.date, true);
+    });
+
+    return preppedData;
+  }
+}
+
+function formatDate(date) {
+  const d = new Date(date);
+  let month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate();
+  const year = d.getFullYear();
+
+  if (month.length < 2) {
+    month = '0' + month;
+  }
+  if (day.length < 2) {
+    day = '0' + day;
+  }
+
+  return [year, month, day].join('-');
+}
