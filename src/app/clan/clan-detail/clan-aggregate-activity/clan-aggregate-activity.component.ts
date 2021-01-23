8157b13ddb87843c260f916e57bfabe3ab69a622
WorthyD <danieljworthy@gmail.com>
2021-01-22 20:19:56 -0600

    Stuff

---
+++ src/app/clan/clan-detail/clan-aggregate-activity/clan-aggregate-activity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clan-aggregate-activity',
+  templateUrl: './clan-aggregate-activity.component.html',
+  styleUrls: ['./clan-aggregate-activity.component.scss']
+})
+export class ClanAggregateActivityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/clan/clan-detail/clan-aggregate-activity/clan-aggregate-activity.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClanAggregateActivityComponent } from './clan-aggregate-activity.component';
+
+
+
+@NgModule({
+  declarations: [ClanAggregateActivityComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ClanAggregateActivityModule { }
