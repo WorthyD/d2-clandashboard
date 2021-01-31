475be1cc8f088a95ee6db6d8654433856578be84
WorthyD <danieljworthy@gmail.com>
2021-01-30 20:17:07 -0600

    updaes

--- src/app/clan/store/member-activities/member-activities.state.ts
+++ src/app/clan/store/member-activities/member-activities.state.ts
@@ -1,19 +1,21 @@
 // // import {ClanDetails} from 'bungie-models';
-// import { MemberActivityStat, MemberActivityStats } from 'bungie-models';
+import { MemberActivityStat, MemberActivityStats } from 'bungie-models';
 
-// import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
+import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
 
-// export interface MemberActivityStatState extends EntityState<MemberActivityStats> {
-//     loaded: boolean;
-//     loading: boolean;
-// }
+export interface MemberActivityStatState extends EntityState<MemberActivityStats> {
+  loaded: boolean;
+  loading: boolean;
+  updating: boolean;
+}
 
-// export const MemberActivityStatAdapter: EntityAdapter<MemberActivityStats> = createEntityAdapter<MemberActivityStats>({
-//     selectId: (cm: MemberActivityStats) => cm.id,
-//     sortComparer: false
-// });
+export const MemberActivityStatAdapter: EntityAdapter<MemberActivityStats> = createEntityAdapter<MemberActivityStats>({
+  selectId: (cm: MemberActivityStats) => cm.id,
+  sortComparer: false
+});
 
-// export const MemberActivityStatInitialState: MemberActivityStatState = MemberActivityStatAdapter.getInitialState({
-//     loaded: true,
-//     loading: false
-// });
+export const MemberActivityStatInitialState: MemberActivityStatState = MemberActivityStatAdapter.getInitialState({
+  loaded: false,
+  loading: false,
+  updating: false
+});
