7d69f2256cf61db4086531e4b0f34bf5941b449b
WorthyD <danieljworthy@gmail.com>
2021-01-31 05:51:38 -0600

    Forgot to save

--- src/app/clan/store/member-activities/member-activities.actions.ts
+++ src/app/clan/store/member-activities/member-activities.actions.ts
@@ -1,33 +1,34 @@
-// import { createAction, props } from '@ngrx/store';
-// import { MemberProfile, MemberActivityStat, MemberActivityStats } from 'bungie-models';
+import { createAction, props } from '@ngrx/store';
+import { MemberProfile, MemberActivityStat, MemberActivityStats } from 'bungie-models';
 
-// const base = '[MemberActivities] - ';
+const base = '[MemberActivities] - ';
 
-// export const loadClanMembersActivities = createAction(
-//     `${base} Load Clan MembersActivities`,
-//     props<{ clanId: number }>()
-// );
+export const loadClanMembersActivities = createAction(
+  `${base} Load Clan MembersActivities`,
+  props<{ clanId: number }>()
+);
+
+// -----------------------------
+export const loadMemberActivities = createAction(`${base} Load MemberActivities`, props<{ member: MemberProfile[] }>());
+
+export const loadMemberActivitiesSuccess = createAction(
+  `${base} Load Clan Members Success`,
+  props<{ memberActivities: MemberActivityStats[] }>()
+);
+
+export const loadClanMemberFailure = createAction(`${base} Load Clan Members Failure`, props<{ error: any }>());
+
+// -----------------------------
+
+export const refreshMemberActivities = createAction(
+  `${base} Update MemberActivities From API`,
+  props<{ member: MemberProfile[] }>()
+);
+
+export const refreshMemberActivitiesComplete = createAction(
+  `${base} Update MemberActivities From API Complete`,
+  props<{ memberActivities: MemberActivityStats[] }>()
+);
 
 
-// export const loadMemberActivities = createAction(
-//     `${base} Load MemberActivities`,
-//     props<{ member: MemberProfile }>()
-// );
-
-// export const loadMemberActivitiesEmpty = createAction(
-//     `${base} Load MemberActivitiesEmpty`
-// );
-
-// // export const loadMemberActivitiesFromAPI = createAction(
-// //     `${base} Load MemberActivities From API`,
-// //     props<{ memberActivities: MemberActivityStats }>()
-// // );
-
-// export const loadMemberActivitiesSuccess = createAction(
-//     `${base} Load Clan Members Success`,
-//     props<{ memberActivities: MemberActivityStats }>()
-// );
-// export const loadClanMemberFailure = createAction(
-//     `${base} Load Clan Members Failure`,
-//     props<{ error: any }>()
-// );
+export const refreshMemberActivitiesFailure = createAction(`${base} refresh Clan Members Failure`, props<{ error: any }>());
