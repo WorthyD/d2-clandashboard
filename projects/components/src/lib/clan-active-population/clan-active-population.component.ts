c3e44d2b7b18acebb0c3b701cc7f2c5b3c1fca87
WorthyD <danieljworthy@gmail.com>
2021-03-13 20:06:34 -0600

    Stuff

--- projects/components/src/lib/clan-active-population/clan-active-population.component.ts
+++ projects/components/src/lib/clan-active-population/clan-active-population.component.ts
@@ -1,17 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { routeAnimations, ROUTE_ANIMATIONS_ELEMENTS } from '../core/animations/route.animations.service';
 
 @Component({
   selector: 'lib-clan-active-population',
   templateUrl: './clan-active-population.component.html',
-  styleUrls: ['./clan-active-population.component.scss'],
-  animations: [routeAnimations]
+  styleUrls: ['./clan-active-population.component.scss']
+  // animations: [routeAnimations]
 })
 export class ClanActivePopulationComponent implements OnInit {
-  routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
-  constructor() { }
+  //  routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
 
-  ngOnInit(): void {
-  }
+  @Input()
+  last24Hours;
+  @Input()
+  prev24Hours;
 
+  @Input()
+  lastWeek;
+  @Input()
+  prevWeek;
+
+  @Input()
+  lastMonth;
+
+  @Input()
+  prevMonth;
+
+
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
