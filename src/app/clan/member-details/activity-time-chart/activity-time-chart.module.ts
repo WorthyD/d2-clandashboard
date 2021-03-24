8c61b60bb50be84d8a07a350dee81a8385cfe7a1
WorthyD <danieljworthy@gmail.com>
2021-03-23 19:07:18 -0500

    Adding new component

---
+++ src/app/clan/member-details/activity-time-chart/activity-time-chart.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivityTimeChartComponent } from './activity-time-chart/activity-time-chart.component';
+
+
+
+@NgModule({
+  declarations: [ActivityTimeChartComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ActivityTimeChartModule { }
---
+++ src/app/clan/member-details/activity-time-chart/activity-time-chart/activity-time-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-time-chart',
+  templateUrl: './activity-time-chart.component.html',
+  styleUrls: ['./activity-time-chart.component.scss']
+})
+export class ActivityTimeChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
