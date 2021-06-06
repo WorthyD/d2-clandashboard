9208c753ac60dc04e80ff605d7dd0c83b1816c48
WorthyD <danieljworthy@gmail.com>
2021-06-05 19:56:38 -0500

    stuff

--- src/app/player-search/player-search.component.ts
+++ src/app/player-search/player-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerSearchService } from './player-search.service';
 
 @Component({
   selector: 'app-player-search',
@@ -6,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-search.component.scss']
 })
 export class PlayerSearchComponent implements OnInit {
+  constructor(private playerSearchService: PlayerSearchService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/player-search/player-search.module.ts
+++ src/app/player-search/player-search.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlayerSearchComponent } from './player-search.component';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { PlayerSearchService } from './player-search.service';
 
 @NgModule({
-  declarations: [
-    PlayerSearchComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [PlayerSearchComponent],
+  imports: [CommonModule, ReactiveFormsModule, MatInputModule, MatFormFieldModule],
+  providers: [PlayerSearchService]
 })
-export class PlayerSearchModule { }
+export class PlayerSearchModule {}
