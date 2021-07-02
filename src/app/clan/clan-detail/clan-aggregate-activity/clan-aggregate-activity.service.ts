cba9d9b645e7a4581a45f2e4dc18586a76050528
WorthyD <danieljworthy@gmail.com>
2021-07-01 20:26:12 -0500

    clean up

--- src/app/clan/clan-detail/clan-aggregate-activity/clan-aggregate-activity.service.ts
+++ src/app/clan/clan-detail/clan-aggregate-activity/clan-aggregate-activity.service.ts
@@ -7,25 +7,16 @@ import {
 } from '@destiny/data';
 import { select } from '@ngrx/store';
 import { Store } from '@ngrx/store';
-import { nowPlusDays } from 'projects/data/src/lib/utility/date-utils';
 
 import { BehaviorSubject, combineLatest } from 'rxjs';
-import { filter, map, switchMap } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 import * as clanIdSelectors from '../../store/clan-id/clan-id.selector';
-import {
-  getAllMembers,
-  getIsMembersProfilesLoaded,
-  getIsMembersProfilesLoading
-} from '../../store/member-profiles/member-profiles.selectors';
+import { getAllMembers, getIsMembersProfilesLoaded } from '../../store/member-profiles/member-profiles.selectors';
 import {
   getAllMemberActivities,
   getClanMemberActivitiesLoaded,
   getClanMemberActivitiesUpdating
 } from '../../store/member-activities/member-activities.selectors';
-// import { groupActivitiesByDate } from 'projects/data/src/lib/utility/group-activity-by-date';
-import { formatDate } from 'projects/data/src/lib/utility/format-date';
-import { groupActivityStatsByDate } from 'projects/data/src/lib/utility/group-activity-by-date';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -72,40 +63,7 @@ export class ClanAggregateActivityService {
     }
   }
 
-  // events = [];
-  // events$ = combineLatest([this.isMembersLoaded$, this.clanId$, this.clanMemberProfiles$]).pipe(
-  //   switchMap(([isMemberLoaded, id, clanMembers]) => {
-  //     this.isLoading = true;
-  //     this.events = [];
-
-  //     return this.service.getClanActivityStatsForDuration(id, clanMembers, nowPlusDays(-30), 0).pipe(
-  //       map((stats) => {
-  //         this.events = stats;
-  //         this.isLoading = false;
-  //       })
-  //     );
-
-  //     // if (selectedMembers.length > 0) {
-  //     //   clanMembers = clanMembers.filter((members) => {
-  //     //     return selectedMembers.indexOf(members.profile.data.userInfo.displayName) > -1;
-  //     //   });
-  //     // }
-  //     // return this.clanActivityService.getClanActivityStats(id, clanMembers, selectedActivity).pipe(
-  //     //   bufferTime(500, undefined, 20),
-  //     //   mergeMap((members) => {
-  //     //     this.activityStats = this.activityStats.concat(members);
-  //     //     return members;
-  //     //   }),
-  //     //   toArray(),
-  //     //   map((stats) => {
-  //     //     this.isLoading = false;
-  //     //   })
-  //     // );
-  //   })
-  // );
-
   load() {
-    //this.events$.subscribe();
     this.events2$.subscribe();
   }
   changeDuration(duration) {
--- src/app/clan/store/member-profiles/member-profiles.effects.ts
+++ src/app/clan/store/member-profiles/member-profiles.effects.ts
@@ -1,72 +1,30 @@
 import { Injectable } from '@angular/core';
 
-import { Actions, Effect, ofType, createEffect } from '@ngrx/effects';
-import { Store, select } from '@ngrx/store';
-import { empty, of, from, forkJoin, BehaviorSubject } from 'rxjs';
+import { Actions, ofType, createEffect } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
 
-// import * as clanDetailActions from './clan-detail.actions';
-// import * as clanMemberActions from './clan-members.actions';
-
-// import { ClanDatabase } from '../../../services/ClanDatabase';
-// import { Updater } from '../../services/updater';
 import { ProfileService } from '@destiny/data';
 
-import { MemberProfile } from 'bungie-models';
-import { getAllMembers } from '../clan-members/clan-members.selectors';
-
 import * as memberProfileActions from './member-profiles.actions';
-import * as clanMemberActions from '../clan-members/clan-members.actions';
 
-import * as clanIdSelectors from '../clan-id/clan-id.selector';
-
-import {
-  catchError,
-  map,
-  switchMap,
-  distinctUntilChanged,
-  first,
-  filter,
-  concatMap,
-  tap,
-  mergeMap,
-  take,
-  withLatestFrom,
-  mergeAll,
-  concatAll,
-  mergeMapTo,
-  toArray,
-  combineAll,
-  bufferTime
-} from 'rxjs/operators';
+import { map, switchMap, mergeMap, toArray, bufferTime } from 'rxjs/operators';
 import { addNotification, updateNotification, removeNotification } from '../notifications/notifications.actions';
 
 @Injectable()
 export class MemberProfileEffects {
-  constructor(
-    private actions$: Actions,
-    private store: Store<any>,
-    // private d2Service: Destiny2Service,
-    // private parser: ClanParseService,
-    private profileService: ProfileService
-  ) {}
-
-  // loadMemberProfiles$ = createEffect(() =>
+  constructor(private actions$: Actions, private store: Store<any>, private profileService: ProfileService) {}
 
   loadProfiles$ = createEffect(() =>
     this.actions$.pipe(
       ofType(memberProfileActions.initLoadMemberProfiles),
       switchMap(({ clanId, clanMembers }) => {
-        // clanMembers = clanMembers.slice(0, 10);
         let progress = 0;
         this.store.dispatch(
           addNotification({ notification: { id: 'memberProfile', title: 'Updating Profiles', data: { progress } } })
         );
 
         return this.profileService.getSerializedProfiles(clanId.toString(), clanMembers).pipe(
-          // tap((x) => {
-          //   this.store.dispatch(memberProfileActions.loadMemberProfile({ memberProfile: x }));
-          // }),
-          bufferTime(500, undefined, 20),
+          bufferTime(1000, undefined, 100),
           /**
            * Don't continue processing if the timer in `bufferTime` was reached and
            *   there are no buffered companies.
