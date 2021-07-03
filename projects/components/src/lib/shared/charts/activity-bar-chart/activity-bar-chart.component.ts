f09f0849d88e9d4e21c610c031dc659454d2d3b2
WorthyD <danieljworthy@gmail.com>
2021-07-02 19:44:45 -0500

    STuff

--- projects/components/src/lib/shared/charts/activity-bar-chart/activity-bar-chart.component.ts
+++ projects/components/src/lib/shared/charts/activity-bar-chart/activity-bar-chart.component.ts
@@ -15,6 +15,18 @@ import * as d3 from 'd3';
 import { SECONDS_IN_HOUR } from '@destiny/models/constants';
 import { PlaytimePipe } from '../../../pipes/playtime/playtime.pipe';
 import { compare } from '../../../utilities/compare';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexDataLabels,
+  ApexFill,
+  ApexLegend,
+  ApexPlotOptions,
+  ApexStroke,
+  ApexTooltip,
+  ApexXAxis,
+  ApexYAxis
+} from 'ng-apexcharts';
 
 @Component({
   selector: 'lib-activity-bar-chart',
@@ -29,12 +41,12 @@ export class ActivityBarChartComponent implements OnInit {
   data;
   x;
   y;
-  tooltip;
+  // tooltip;
 
-  chartHeight = 100;
-  chartWidth = 1000;
-  threshHold = SECONDS_IN_HOUR * 20;
-  color = d3.scaleLinear().range(['#00ff00', '#ff0000']).domain([0, 1]);
+  // chartHeight = 100;
+  // chartWidth = 1000;
+  // threshHold = SECONDS_IN_HOUR * 20;
+  // color = d3.scaleLinear().range(['#00ff00', '#ff0000']).domain([0, 1]);
 
   formatPipe = new PlaytimePipe();
 
@@ -60,10 +72,49 @@ export class ActivityBarChartComponent implements OnInit {
   @Input()
   endDate: Date;
 
-  constructor(private elRef: ElementRef, private cd: ChangeDetectorRef) {
-    this.hostElement = this.elRef.nativeElement;
-    this.cd.detach();
+  // ----------------
+  series: ApexAxisChartSeries = [];
+  chart: ApexChart = { type: 'bar', height: 400 };
+  yaxis: ApexYAxis = {
+    title: {
+      //text: '$ (thousands)'
+    }
+  };
+  xaxis: ApexXAxis = {
+    type: 'datetime'
+  };
+  legend: ApexLegend = {};
 
+  plotOptions: ApexPlotOptions = {
+    bar: {
+      horizontal: false,
+      columnWidth: '75%'
+      // endingShape: 'rounded'
+    }
+  };
+  dataLabels: ApexDataLabels = {
+    enabled: false
+  };
+  fill: ApexFill = {
+    opacity: 1
+  };
+  stroke: ApexStroke = {
+    show: true,
+    width: 2,
+    colors: ['transparent']
+  };
+
+  tooltip: ApexTooltip = {
+    y: {
+      formatter: function (val) {
+        return val + ' seconds';
+      }
+    }
+  };
+
+  constructor(private elRef: ElementRef, private cd: ChangeDetectorRef) {
+    // this.hostElement = this.elRef.nativeElement;
+    // this.cd.detach();
     const date = new Date();
     const offset = date.getDay() >= 2 ? 2 : -5;
     this.endDate = new Date(date.setDate(date.getDate() - date.getDay() + offset));
@@ -71,86 +122,92 @@ export class ActivityBarChartComponent implements OnInit {
   }
 
   private updateChart(eventData) {
-    if (!this.svg) {
-      this.createChart(eventData);
-      return;
-    }
+    // if (!this.svg) {
+    //   this.createChart(eventData);
+    //   return;
+    // }
+    this.processData(eventData);
   }
 
   ngOnInit(): void {}
 
-  private createChart(eventData) {
-    this.removeExistingChartFromParent();
-    this.setChartDimentions();
+  // private createChart(eventData) {
+  //   this.removeExistingChartFromParent();
+  //   this.setChartDimentions();
 
-    this.addToolTip();
+  //   this.addToolTip();
 
-    this.processData(eventData);
-  }
+  //   this.processData(eventData);
+  // }
 
-  private removeExistingChartFromParent() {
-    // !!!!Caution!!!
-    // Make sure not to do;
-    //     d3.select('svg').remove();
-    // That will clear all other SVG elements in the DOM
-    d3.select(this.hostElement).select('svg').remove();
-  }
+  // private removeExistingChartFromParent() {
+  //   // !!!!Caution!!!
+  //   // Make sure not to do;
+  //   //     d3.select('svg').remove();
+  //   // That will clear all other SVG elements in the DOM
+  //   d3.select(this.hostElement).select('svg').remove();
+  // }
 
-  private setChartDimentions() {
-    this.svg = d3
-      .select(this.hostElement)
-      .attr('class', 'activity-heatmap')
-      .append('svg')
-      .attr('width', '100%')
-      .attr('viewBox', `0 0 ${this.chartWidth} ${this.chartHeight}`)
-      .append('g');
-//    this.x = d3.scaleBand().range([0, this.chartWidth], 0.05);
-    this.x = d3.scaleTime().range([0, this.chartWidth]);
-    this.y = d3.scaleLinear().range([this.chartHeight, 0]);
-  }
-  private addToolTip() {
-    this.tooltip = d3.select(this.hostElement).append('div').attr('class', 'activity-tooltip');
-  }
+  // private setChartDimentions() {
+  //   this.svg = d3
+  //     .select(this.hostElement)
+  //     .attr('class', 'activity-heatmap')
+  //     .append('svg')
+  //     .attr('width', '100%')
+  //     .attr('viewBox', `0 0 ${this.chartWidth} ${this.chartHeight}`)
+  //     .append('g');
+  //   //    this.x = d3.scaleBand().range([0, this.chartWidth], 0.05);
+  //   this.x = d3.scaleTime().range([0, this.chartWidth]);
+  //   this.y = d3.scaleLinear().range([this.chartHeight, 0]);
+  // }
+  // private addToolTip() {
+  //   this.tooltip = d3.select(this.hostElement).append('div').attr('class', 'activity-tooltip');
+  // }
   private processData(sourceData) {
     if (sourceData) {
       const cleanedData = this.prepData(sourceData);
+      this.series = [
+        {
+          name: '',
+          data: cleanedData
+        }
+      ];
+
       // this.x.domain(
       //   cleanedData.map(function (d) {
       //     return d.date;
       //   })
       // );
-      this.x.domain([cleanedData[0].date, cleanedData[cleanedData.length - 1].date]);
-      this.y.domain([0, this.threshHold]);
+      // this.x.domain([cleanedData[0].date, cleanedData[cleanedData.length - 1].date]);
+      // this.y.domain([0, this.threshHold]);
 
-      const bars = this.svg.selectAll('bar').data(cleanedData).enter().append('rect');
+      // const bars = this.svg.selectAll('bar').data(cleanedData).enter().append('rect');
 
-      bars
-        .attr('class', 'activity-bar')
-        .attr('x', (d) => {
-          return this.x(d.date);
-        })
-        .attr('y', (d) => {
-          return this.y(d.seconds);
-        })
-        //.attr('width', this.x.bandwidth())
-        .attr('width', this.chartWidth / cleanedData.length)
-        .attr('height', (d) => {
-          return this.chartHeight - this.y(d.seconds);
-        });
+      // bars
+      //   .attr('class', 'activity-bar')
+      //   .attr('x', (d) => {
+      //     return this.x(d.date);
+      //   })
+      //   .attr('y', (d) => {
+      //     return this.y(d.seconds);
+      //   })
+      //   //.attr('width', this.x.bandwidth())
+      //   .attr('width', this.chartWidth / cleanedData.length)
+      //   .attr('height', (d) => {
+      //     return this.chartHeight - this.y(d.seconds);
+      //   });
 
-      bars
-        .on('mouseover', (d) => {
-          this.tooltip.style('opacity', 0.9);
-          this.tooltip.html(
-            `Week Starting: ${formatDate(d.date)}<br/> Time:  ${this.formatPipe.transform(d.seconds)}`
-          );
-        })
-        .on('mousemove', () => {
-          this.tooltip.style('left', d3.event.pageX + 30 + 'px').style('top', d3.event.pageY + 'px');
-        })
-        .on('mouseout', (d) => {
-          this.tooltip.style('opacity', 0);
-        });
+      // bars
+      //   .on('mouseover', (d) => {
+      //     this.tooltip.style('opacity', 0.9);
+      //     this.tooltip.html(`Week Starting: ${formatDate(d.date)}<br/> Time:  ${this.formatPipe.transform(d.seconds)}`);
+      //   })
+      //   .on('mousemove', () => {
+      //     this.tooltip.style('left', d3.event.pageX + 30 + 'px').style('top', d3.event.pageY + 'px');
+      //   })
+      //   .on('mouseout', (d) => {
+      //     this.tooltip.style('opacity', 0);
+      //   });
     }
   }
 
@@ -165,33 +222,33 @@ export class ActivityBarChartComponent implements OnInit {
         break;
       }
       if (d >= this.startDate) {
-        const dFormatted = formatDate(d);
+        //const dFormatted = formatDate(d);
 
         const data = sourceData.find((x) => x.date.toDateString() === d.toDateString());
 
         if (data) {
-          preppedData.push(data);
+          preppedData.push({ x: data.date, y: data.seconds });
         } else {
-          preppedData.push({ date: d, seconds: 0 });
+          preppedData.push({ x: d, y: 0 });
         }
       }
     }
 
     preppedData.sort((a, b) => {
-      return compare(a.date, b.date, true);
+      return compare(a.x, b.x, true);
     });
 
     return preppedData;
-    return sourceData
-      .sort((a, b) => {
-        return compare(a.date, b.date, true);
-      })
-      .map((x) => {
-        return {
-          date: new Date(x.date),
-          seconds: x.seconds
-        };
-      });
+    // return sourceData
+    //   .sort((a, b) => {
+    //     return compare(a.date, b.date, true);
+    //   })
+    //   .map((x) => {
+    //     return {
+    //       date: new Date(x.date),
+    //       seconds: x.seconds
+    //     };
+    //   });
   }
 }
 
