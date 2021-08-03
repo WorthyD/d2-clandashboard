e0dc82dfb18b65904ed71ce15a3d26050e195343
WorthyD <danieljworthy@gmail.com>
2021-08-02 19:16:58 -0500

    Updates

---
+++ projects/components/src/lib/seals/player-seals/member-profile.converter.ts
@@ -0,0 +1,5 @@
+import { MilestoneProfiles } from '@destiny/data';
+import { MemberProfile, PresentationNodeDefinition } from 'bungie-models';
+import { PlayerSeal } from './player-seal';
+
+export function convertSealAndProfile(seals: PresentationNodeDefinition, records: MilestoneProfiles) {}
--- projects/components/src/lib/seals/player-seals/player-seals.module.ts
+++ projects/components/src/lib/seals/player-seals/player-seals.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlayerSealsComponent } from './player-seals.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
 
 const components = [PlayerSealsComponent];
 
 @NgModule({
   declarations: [...components],
   exports: [...components],
-  imports: [CommonModule]
+  imports: [CommonModule, MatCardModule, MatProgressBarModule]
 })
 export class PlayerSealsModule {}
