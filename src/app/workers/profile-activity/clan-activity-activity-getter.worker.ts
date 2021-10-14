095dbab26ad554aec4c03cb987a47e8777e66041
WorthyD <danieljworthy@gmail.com>
2021-10-14 06:59:46 -0500

    Stuff

--- src/app/workers/profile-activity/clan-activity-activity-getter.worker.ts
+++ src/app/workers/profile-activity/clan-activity-activity-getter.worker.ts
@@ -1,16 +1,28 @@
 import { ClanMemberActivityService } from 'projects/data/src/lib/clan-db/clan-member-activity/clan-member-activity.service';
 
 import { ClanDatabase } from 'projects/data/src/lib/clan-db/ClanDatabase';
-import { take } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 
 addEventListener('message', ({ data }) => {
   const clanDatabase = new ClanDatabase();
   const profileService = new ClanMemberActivityService(clanDatabase, data.apiKey);
 
-
+  console.log(data.memberProfiles);
   profileService
     .getAllActivitiesFromCache2(data.clanId, data.memberProfiles, data.activityId)
-    .pipe(take(1))
+    .pipe(
+      take(1),
+      map((x) => {
+        return x.map((y) => {
+          return {
+            ...y,
+            profileName: data.memberProfiles.find(
+              (p) => `${p.profile.data.userInfo.membershipType}-${p.profile.data.userInfo.membershipId}` === y.id
+            )?.profile.data.userInfo.displayName
+          };
+        });
+      })
+    )
     .subscribe((x) => {
       postMessage({ type: 'complete', data: x });
     });
