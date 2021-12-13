a19408c6a60eb24ff539b41e6bebb61cfcf369e6
WorthyD <danieljworthy@gmail.com>
2021-12-12 18:50:27 -0600

    Stuff

---
+++ projects/bungie-models/src/lib/entities/dungeons/grasp.ts
@@ -0,0 +1,21 @@
+import { Activity } from '../../models/ActivityStats';
+
+export const GraspOfAvarice: Activity = {
+  hashes: [4078656646],
+  key: 'grasp',
+  abbreviatedName: 'Grasp',
+  displayName: 'Grasp of Avarice',
+  sortOrder: 13,
+  isGuidedGames: false,
+  isVaulted: false
+};
+
+export const GraspOfAvariceMaster: Activity = {
+  hashes: [3774021532],
+  key: 'graspm',
+  abbreviatedName: 'Grasp - Master',
+  displayName: 'Grasp of Avarice Master',
+  sortOrder: 14,
+  isGuidedGames: false,
+  isVaulted: false
+};
