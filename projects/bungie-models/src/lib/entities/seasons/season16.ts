5119210278779085c8356cf55217b3ae29e0bcbd
WorthyD <danieljworthy@gmail.com>
2022-03-12 17:16:25 -0600

    Stuff

---
+++ projects/bungie-models/src/lib/entities/seasons/season16.ts
@@ -0,0 +1,10 @@
+import { Season } from './season';
+
+export const season16: Season = {
+  name: 'Season of the Risen',
+  valorLegendHash: 0,
+  seasonRewardProgressionHash: 2069932355,
+  seasonPrestigeProgressionHash: 1787069365,
+  startDate: new Date('2022-02-22T17:00:00Z'),
+  endDate: new Date('2023-02-22T17:00:00Z')
+};
