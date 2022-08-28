18d92269e3f19d7cf19ef0901e4f7c8552c3dd9d
WorthyD <danieljworthy@gmail.com>
2022-08-28 16:20:24 -0500

    Adding missing files

---
+++ projects/bungie-models/src/lib/entities/dungeons/duality.ts
@@ -0,0 +1,21 @@
+import { Activity } from '../../models/ActivityStats';
+
+export const Duality: Activity = {
+  hashes: [2823159265],
+  key: 'dual',
+  abbreviatedName: 'Duality',
+  displayName: 'Duality',
+  sortOrder: 15,
+  isGuidedGames: false,
+  isVaulted: false
+};
+
+export const DualityMaster: Activity = {
+  hashes: [1668217731],
+  key: 'dualm',
+  abbreviatedName: 'Duality - Master',
+  displayName: 'Grasp of Avarice Master',
+  sortOrder: 16,
+  isGuidedGames: false,
+  isVaulted: false
+};
---
+++ projects/bungie-models/src/lib/entities/seasons/season17.ts
@@ -0,0 +1,10 @@
+import { Season } from './season';
+
+export const season17: Season = {
+  name: 'Season of the Haunted',
+  valorLegendHash: 0,
+  seasonRewardProgressionHash: 26079066,
+  seasonPrestigeProgressionHash: 482365574,
+  startDate: new Date('2022-05-24T17:00:00Z'),
+  endDate: new Date('2022-08-23T17:00:00Z')
+};
